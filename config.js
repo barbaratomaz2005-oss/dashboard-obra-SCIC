d26aa179ed154d5fb64b72558ad2cb877b3e75a7
barbaratomaz2005-oss <barbaratomaz2005@gmail.com>
2026-07-20 22:22:50 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,16 @@
 const SHEET_ID = "15elqBXpHgUMZu9GCxOuQzeHfYvb7Zsv0QVWl0FfgRwc";
 
 const GIDS = {
-  Config: "295667101",
+    config: "295667101",
+    avancoFisicoSemanal: "873446954",
+    avancoFisicoAcumulado: "1809824478",
+    atividadesSemanaisFotos: "376787791",
+    atividadesSemanaisPontos: "1332500257",
+    fluxoFinanceiroSemanal: "1676535506",
+    fluxoFinanceiroTrimestral: "173394064",
+    curvaS: "334216643",
+    contratacoes: "1718836335",
+    concessionarias: "723176947",
+    canteiroFuncionarios: "788302874",
+    rocontec: "1459457304"
 };
